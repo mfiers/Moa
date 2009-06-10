724fbf17d2feeaf0ddcb91a29c163fa36edd25cd
Mark Fiers <mark.fiers.42@gmail.com>
2009-06-10 12:32:48 +0100

    improved upload mechanism. much cleaner now

--- template/upload2gbrowseInclude.mk
+++ template/upload2gbrowseInclude.mk
@@ -1,4 +1,3 @@
-#
 # upload to gbrowse, only used as an extension to an analysis
 # i.e. should be included by another makefile
 # 
@@ -13,61 +12,69 @@ moa_description_upload2gbrowse = Upload data to a seqfeature gbrowse database
 ################################################################################
 ## Definitions
 ## targets that the enduser might want to use
-moa_targets += gbrowse_upload gbrowse_clean
-gbrowse_upload_help = Upload data to gbrowse
-gbrowse_clean = clean all datat from this analysis from the gbrowse database
-moa_must_define += gbrowse_user gbrowse_db
-gbrowse_user_help = gbrowse db user
-gbrowse_db_help = gbrowse database
+moa_targets += upload
+upload_help = upload all new data to gbrowse
 
-## Define functions to determine id & source - helps in deleting stuff from the db 
-## Not sure if we really need this!
-get_id ?= basename $$file .$(input_extension)
-get_source ?= echo $(gff_source)
+moa_may_define += gbrowse_user gbrowse_db
+gbrowse_user_help = gbrowse db user. If not defined, this defaults to 'moa'.
+gbrowse_db_help = gbrowse database. If not defined, this defaults to 'moa'.
 
-upload_touch_files = $(addprefix upload/, $(notdir $(gbrowse_upload_files)))
+moa_may_define += gbrowse_do_upload
+gbrowse_do_upload_help = (T/F) Unless this value is T, the data will not be uploaded \
+  to a gbrowse database
 
-#define the functions to actually delete the stuff. I don't think these will need
-#to be overidden very often, but, at least it is possible now.
-delete_function ?= bp_seqfeature_delete.pl -d $(gbrowse_db) -u $(gbrowse_user) -n $$id
-delete_all_function ?= bp_seqfeature_delete.pl -d $(gbrowse_db) -u $(gbrowse_user) -t '$$method:$$source'
+-include $(shell echo $$MOABASE)/template/moaBase.mk
 
 gup_input_files = $(wildcard $(gup_input_dir)/*.$(gup_input_extension))
-gup_touch_files = $(addprefix upload/, $(notdir $(gup_input_files)))
+#gup_touch_files = $(addprefix upload/, $(notdir $(wildcard $(gup_input_dir)/*.$(gup_input_extension))))
 
-## Shortcut for gbrowse_upload.. which is way too long
-.PHONY: upl
-upl: gbrowse_upload
+gbrowse_user ?= moa
+gbrowse_db ?= moa
+gbrowse_do_upload ?= F
 
-.PHONY: gbrowse_upload
-gbrowse_upload: gbrowse_upload_prepare $(gup_touch_files) gbrowse_batch_upload
-	@echo "finish gbrowse_upload"
+# function to delete the results of one, a few and all input files
+# if sticking to a few rules this should work for most analyses.
+gup_delete_single ?= bp_seqfeature_delete.pl -d $(gbrowse_db) -u $(gbrowse_user) \
+  -n $(gff_source)_`basename $< .gff`
+gup_delete_few ?= bp_seqfeature_delete.pl -d $(gbrowse_db) -u $(gbrowse_user) \
+  -n $(addprefix $(gff_source)_, $(shell for f in $?; do basename $$f .$(gup_input_extension); done))
+#gup_delete_all ?= bp_seqfeature_delete.pl -d $(gbrowse_db) -u $(gbrowse_user) \
+#  -n $(addprefix $(gff_source)_, $(shell for f in $(gup_input_files); do basename $$f .$(gup_input_extension); done))
+gup_delete_all ?= bp_seqfeature_delete.pl -d $(gbrowse_db) -u $(gbrowse_user) \
+  -t '*:$(gff_source)'
 
-.PHONY: gbrowse_batch_upload
-gbrowse_batch_upload: $(gup_input_files)
-	@echo all $^
-	@echo new $?
-	echo "uploading $?"
-	bp_seqfeature_load.pl -d $(gbrowse_db) -u $(gbrowse_user) -f $?
-	touch gbrowse_batch_upload
+# the default upload target runs make upload. This is to make sure that possible files
+# created by the rest of this makefile are read correctly. I'm under the impression
+# that this should be possible by secondary expanssion. but could not get this to
+# work properly. If anybody has a bright idea.. please.
 
-gbrowse_upload_prepare:
-	-mkdir upload# remaking the touch file - thus the gff file is either newer than the touch
-# file or the touch file does not exist yet. In the latter case, just create
-# the touch file else, delete data from the db.
-upload/%: $(gup_input_dir)/%
-	echo "Looking at : $@ and $<"
-	@if [[ -f "$@" ]]; then \
-		echo "deleting gbrowse data for $<" ;\
-			echo Execution: $(gup_delete_single_f) ;\
-			$(gup_delete_single) ;\
+.PHONY: upload2gbrowse_prepare
+upload2gbrowse_prepare: 
+
+.PHONY: upload2gbrowse
+upload2gbrowse:
+	if [ "$(gbrowse_do_upload)" == "T" ]; then \
+		$(MAKE) upload ;\
 	fi
-	touch $@gbrowse_upload_clean:
-	-rm -rf upload
 
+.PHONY: upload2gbrowse_post
+upload2gbrowse_post: 
 
-status_upload2gbrowse:
-	@echo "Gbrowse upload status"
-	@echo "Files to upload: $(words $(gbrowse_upload_files))"
-	@echo "First file to upload: $(word 1, $(gbrowse_upload_files))"
+## Do the real upload
+.NOTPARALLEL: upload
+upload: $(gup_input_files)
+	@echo "Uploading $(words $?) files"
+	@echo $(gup_delete_few)
+	$(gup_delete_few)
+	bp_seqfeature_load.pl -d $(gbrowse_db) -u $(gbrowse_user) -f $?
+	touch upload
+
+.PHONY: upload2gbrowse_clean
+upload2gbrowse_clean:
+	@echo "Run upload2gbrowse_clean"
+	-rm -r upload
+	@echo "Executing $(gup_delete_all)"
+	@echo -e "$(warn_on)Problem$(warn_off) - Not sure if bioperl is buggy here"
+	@echo -e "$(warn_on)Problem$(warn_off) - features might not have been deleted from the db"
+	$(delete_all_function)
 
