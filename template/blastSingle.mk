5ac1cb2f6fbd5aa1098c7d83001c74e534fd8055
Mark Fiers <mark.fiers.42@gmail.com>
2009-07-31 22:23:05 +1200

    cleaning up

--- template/blastSingle.mk
+++ template/blastSingle.mk
@@ -1,3 +1,23 @@
+#
+#    Copyright 2009 Mark Fiers
+#
+#    This file is part of Moa 
+#
+#    Moa is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Moa is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Moa.  If not, see <http://www.gnu.org/licenses/>.
+#
+#    See: http://github.com/mfiers/Moa/
+#
 #this is a small adaptation of the blast template, expecting one single input file
 #without escaping the following line would have read:
 blast_input_dir = $(shell x=$(blast_input_file); echo $${x%/*})
--- template/gmapdb.mk
+++ template/gmapdb.mk
@@ -1,4 +1,23 @@
-# Build a Gmap database
+#
+#    Copyright 2009 Mark Fiers
+#
+#    This file is part of Moa 
+#
+#    Moa is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Moa is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Moa.  If not, see <http://www.gnu.org/licenses/>.
+#
+#    See: http://github.com/mfiers/Moa/
+## Build a Gmap database
 
 ################################################################################
 # Variable checks & definition & help
