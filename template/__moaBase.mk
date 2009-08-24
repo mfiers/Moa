ead703f477d0fe7a1e337d35494d661ad49b3e69
Mark Fiers <mark.fiers.42@gmail.com>
2009-08-25 09:41:01 +1200

    updated license text

--- template/__moaBase.mk
+++ template/__moaBase.mk
@@ -1,31 +1,21 @@
-#
-#    Copyright 2009 Mark Fiers
-#
-#    This file is part of Moa 
-#
-#    Moa is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    Moa is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with Moa.  If not, see <http://www.gnu.org/licenses/>.
-#
-#    See: http://github.com/mfiers/Moa/
-###
-## Moabase - base library for all moa styled makefiles.
-##
-## Include this after all moa style definitions but before
-## defining any target or (and this is important) any variable
-## that depends on a variable defined in moa.mk
-##
-
-## we use bash!
+# 
+# Copyright 2009 Mark Fiers, Plant & Food Research
+# 
+# This file is part of Moa - http://github.com/mfiers/Moa
+# 
+# Moa is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+# 
+# Moa is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+# License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with Moa.  If not, see <http://www.gnu.org/licenses/>.
+# 
 SHELL := /bin/bash
 
 ## We use the Gnu Make Standard Library
--- template/varscan.mk
+++ template/varscan.mk
@@ -1,28 +1,21 @@
-# Run Varscan
-#
-#    Copyright 2009 Mark Fiers
-#
-#    This file is part of Moa 
-#
-#    Moa is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    Moa is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with Moa.  If not, see <http://www.gnu.org/licenses/>.
-#
-#    See: http://github.com/mfiers/Moa/
-#
-
-################################################################################
-
-# Variable checks & definition & help
+# 
+# Copyright 2009 Mark Fiers, Plant & Food Research
+# 
+# This file is part of Moa - http://github.com/mfiers/Moa
+# 
+# Moa is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+# 
+# Moa is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+# License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with Moa.  If not, see <http://www.gnu.org/licenses/>.
+# 
 moa_ids += varscan 
 moa_title_varscan = Varscan
 moa_description_varscan = Run VARSCAN to detect snps
