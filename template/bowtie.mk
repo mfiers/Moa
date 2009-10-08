fcc50a8f1197cca20205fd2d78c79e21027ec4dd
Mark Fiers <mark.fiers@plantandfood.co.nz>
2009-10-09 12:48:41 +1300

    much improved, handles paired reads now

--- template/bowtie.mk
+++ template/bowtie.mk
@@ -28,7 +28,7 @@ bowtie_db_default_attrib = bowtiedb
 
 moa_must_define += bowtie_input_dir
 bowtie_input_dir_help = input dir with the query files
-
+bowtie_input_dir_default_attrib = pwd
 moa_may_define += bowtie_input_extension
 bowtie_input_extension_help = Extension of the input files, \
   defaults to fastq
@@ -40,8 +40,30 @@ bowtie_input_format_help = Format of the input files, defaults \
 moa_may_define += bowtie_extra_params
 bowtie_extra_params_help = extra parameters to feed bowtie
 
+moa_may_define += bowtie_paired_ends
+bowtie_paired_ends_help = perform a paired end analysis. If so, the	\
+input files are expected to be of the form							\
+ '\*_1.$(bowtie_input_extension)' and 								\
+'\*_2.$(bowtie_input_extension)'
+
 moa_may_define += bowtie_output_name
-bowtie_output_name_gelp = output file name, defaults to 'output'
+bowtie_output_name_help = output file name, defaults to 'output'
+
+moa_may_define += bowtie_insertsize
+bowtie_insertsize_help = Expected insertsize
+
+moa_may_define += bowtie_insertsize_sed
+bowtie_insertsize_sed_help += A sed expression that filters the insert size	\
+from the input file name. Ignored if bowtie_insertsize is defined.s
+
+moa_may_define += bowtie_insertsize_min bowtie_insertsize_max
+bowtie_insertsize_min_help = multiplier determining the minimal		\
+acceptable value for two paired reads to be apart. If the			\
+bowtie_insertsize is 10000 and this parameter is set at 0.8, than	\
+reads that are closer together than 8000 nt are rejected
+bowtie_insertsize_max_help += as bowtie_insert_min, but setting a max	\
+insert size
+
 
 ifndef dont_include_moabase
 	include $(shell echo $$MOABASE)/template/moaBase.mk
@@ -53,6 +75,8 @@ endif
 bowtie_input_extension ?= fastq
 bowtie_input_format ?= fastq
 bowtie_output_name ?= output
+
+bowtie_paired_ends ?= F
 
 moa_register_extra += bowtie_output
 moa_register_bowtie_output = $(shell echo `pwd`)/$(bowtie_output_name)
@@ -67,8 +91,18 @@ ifndef bowtie_input_format_param
 $(error Invalid input format)
 endif
 
+ifeq ($(bowtie_paired_ends),T) 
+bowtie_input_files = $(wildcard $(bowtie_input_dir)/*_1.$(bowtie_input_extension))
+bowtie_output_files = $(addprefix p_,\
+		$(patsubst %_1.$(bowtie_input_extension), %, $(notdir $(bowtie_input_files))))
+else
 bowtie_input_files = $(wildcard $(bowtie_input_dir)/*.$(bowtie_input_extension))
+bowtie_output_files = $(addprefix u_,\
+	$(patsubst %.$(bowtie_input_extension), %, $(notdir $(bowtie_input_files))))
+endif
 
+# -u 30 --solexa1.3-quals 
+# 
 .PHONY: bowtie_prepare
 bowtie_prepare:
 
@@ -76,19 +110,32 @@ bowtie_prepare:
 bowtie_post: 
 
 test:
-	@echo $(bowtie_input_files)
+	@echo "INPUT"
+	@for x in $(bowtie_input_files); do echo $$x; done
+	@echo "OUTPUT"
+	@for x in $(bowtie_output_files); do echo $$x; done
 
 comma:=,
 .PHONY: bowtie
-bowtie: $(bowtie_output_name) 
+bowtie: $(bowtie_output_files)
+	@echo Processed $(bowtie_output_files)
 
-$(bowtie_output_name): $(bowtie_input_files)
-	bowtie $(bowtie_input_format_param) \
-		-p $(MOA_PROCESSORS) \
-		$(bowtie_extra_params) \
-		$(bowtie_db) \
-		$(call merge,$(comma),$^) \
-		output
+
+u_%: $(bowtie_input_dir)/%.$(bowtie_input_extension)
+	echo bowtie $(bowtie_input_format_param) \
+		$(bowtie_extra_params) $(bowtie_db) $< $@
+
+imn=$(bowtie_insertsize_min)
+imx=$(bowtie_insertsize_max)
+p_%: $(bowtie_input_dir)/%_1.$(bowtie_input_extension) $(bowtie_input_dir)/%_2.$(bowtie_input_extension)
+	@IS="$(bowtie_insertsize)";\
+		sizeDef="";\
+		[ "$$IS" ] || IS=`echo "$*" | sed "$(bowtie_insertsize_sed)"`;\
+		[ "$$IS" ] && sizeDef=`python -c "print \"-I %d -X %d\" % ($$IS * $(imn), $$IS*$(imx))"`;\
+		$(call echo, Executing bowtie for $<);\
+		bowtie $(bowtie_input_format_param) \
+			$(bowtie_extra_params) $$sizeDef $(bowtie_db) \
+			 -1 $(word 1,$^) -2 $(word 2,$^) $@
 
 bowtie_clean:
 	-rm -f $(bowtie_output_name)
