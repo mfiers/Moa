12b26335157bab8c63d23a6a8a9a5564f35027f0
Mark Fiers <mark.fiers.42@gmail.com>
2009-10-19 07:53:18 +1300

    rearranging stuff

--- demo/bacterial.annotation/05.databases/10.swissprot.bacteria/10.download/moa.mk
+++
@@ -1,8 +0,0 @@
-lftp_url=ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/taxonomic_divisions/uniprot_sprot_bacteria.dat.gz
-lftp_mode=get
-lftp_timestamp=F
-jid=moa_lftp_10.swissprot.bacteria_3d19591c95ab
-g_input_dir=.
-g_input_pattern=*.gz
-g_name_sed=s/.dat.gz//
-g_process=zcat $< | seqret swissprot::stdin fasta:$(g_target)
--- demo/bacterial.annotation/20.blast/10.glimmer.vs.spbacteria/moa.mk
+++ demo/bacterial.annotation/20.blast/10.glimmer.vs.spbacteria/moa.mk
@@ -1,7 +1,7 @@
-blast_db=../../05.databases/10.swissprot.bacteria/20.blastdb/uniprot_sprot_bacteria
 blast_gff_source=blast.vs.sp.bacteria
 blast_input_dir=../../10.gene.prediction/10.glimmer3/fasta/
 blast_program=blastx
 blast_nohits=10
 project=demo.bacterial.annotation
 jid=moa_blast_10.glimmer.vs.spbacteria_a77c7e81e3a8
+blast_db=../../05.databases/10.ecoli.proteome/20.blastdb/ecoli_proteome
