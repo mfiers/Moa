f272a4e42392ae9e73c53faf5b3d66427797f6d0
Mark Fiers <mf@schicht.(none)>
2009-12-02 21:34:20 +1300

    did not result in any hits :(

--- demo/introduction/10.download/20.swissprot.solanaceae/moa.mk
+++
@@ -1,4 +0,0 @@
-project=introduction
-title=download solanaceae swissprot genes
-lftp_get_name=solanaceae.fasta
-lftp_url=http://www.uniprot.org/uniprot/?query=solanaceae+AND+reviewed%3ayes+taxonomy%3a4070&force=yes&format=fasta
