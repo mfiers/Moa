b590e538af2ba0ba59d65a2beddebab81a9ebd4f
Mark Fiers <mf@schicht.(none)>
2009-12-02 17:20:32 +1300

    download solanceae genes from swissprot

---
+++ demo/introduction/10.download/20.swissprot.solanaceae/moa.mk
@@ -0,0 +1,4 @@
+project=introduction
+title=download solanaceae swissprot genes
+lftp_get_name=solanaceae.fasta
+lftp_url=http://www.uniprot.org/uniprot/?query=solanaceae+AND+reviewed%3ayes+taxonomy%3a4070&force=yes&format=fasta
