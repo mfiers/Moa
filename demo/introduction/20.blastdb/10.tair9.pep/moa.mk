af293980e341f48e870ef04cd06ba332f6993aeb
Mark Fiers <mark.fiers.42@gmail.com>
2009-12-09 11:34:30 +1300

    working on the introduction

--- demo/introduction/20.blastdb/10.tair9.pep/moa.mk
+++
@@ -1,4 +0,0 @@
-title=create a blast db for the arabidopsis tair9 peptides
-bdb_name=tair9
-bdb_fasta_file=../../10.download/30.arabidopsis.proteome/TAIR9_pep_20090619
-bdb_protein=T
--- demo/introduction/30.blast/10.bacs.vs.tair9/moa.mk
+++ demo/introduction/30.blast/10.bacs.vs.tair9/moa.mk
@@ -1,6 +1,6 @@
 title=blast the bacs vs arabidopsis tair9 proteins
 blast_input_dir=../../10.download/10.bac/fasta/
 blast_program=blastx
-blast_db=../../20.blastdb/20.tair9.pep/tair9
-blast_db_default_attrib=blastdb
 blast_eval=100
+blast_db=../../20.blastdb/20.tair9.pep.db/tair9_filtered.phr
+blast_db_default_attrib=blastdb
