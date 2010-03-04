078b5b2110a49a9c867fab052c7e72ce05771d9d
Mark Fiers <mark.fiers.42@gmail.com>
2010-03-04 15:58:01 +1300

    initial commit of a git versioning plugin

---
+++ template/moa/plugins/__git.mk
@@ -0,0 +1,32 @@
+# 
+# Copyright 2009 Mark Fiers, Plant & Food Research
+# 
+# This file is part of Moa - http://github.com/mfiers/Moa
+# 
+# Moa is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+# 
+# Moa is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+# License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with Moa.  If not, see <http://www.gnu.org/licenses/>.
+# 
+
+MOA_INCLUDE_PLUGIN_GIT= yes
+
+################################################################################
+## Git - use git to keep track of project history
+
+## Add a hook for project_init
+moa_hooks_postinit_project += moa_git_initproject
+
+.PHONY: moa_git_initproject
+moa_git_initproject:
+	$e git status 2>&1 | grep -q 'Not a git repo' || \
+		$(call exer,We are already in a GIT repository - cannot init git)
+	echo "hi"
