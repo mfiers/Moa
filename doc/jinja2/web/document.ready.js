274bb50458b668209fcb14402b7e6a0e1e3394e8
Mark Fiers <mark.fiers.42@gmail.com>
2010-05-07 21:34:37 +1200

    new templates for the autoindex gui

---
+++ doc/jinja2/web/document.ready.js
@@ -0,0 +1,62 @@
+$(document).ready(function() {
+    //Remove the load-in-progress window
+    $("#loadInProgress").detach();
+
+    //fix the table - widht 100 percent
+    $("table:first").css("width", "100%%");
+
+    //Extract the header & move it to thead
+    var fr = $("table:first > tbody > tr:first").detach();
+
+    //Remove the first row (link to parent dir)
+    $("table:first > tbody > tr:first").remove();
+    $("table:first").prepend("<thead></thead>");
+    $("table:first > thead").append(fr);
+    
+    // remove the 4th column - some sort of a remains of the
+    // description?? 
+    $("table:first > tbody > tr").each(function(index) {
+        $(this).contents().eq(4).remove();
+        });
+
+    //Remove the icon links (why again??)
+    $("table:first > tbody > tr > td:first-child > a").removeAttr('href');
+    $("table:first > tbody > tr > td:nth-child(2) > a:not([href$='/'])").attr('target', '_blank');
+
+    
+        /*var t = $(this)
+        var icon = t.contents('td:first > a').html();
+        t.contents('td:first').html(icon);
+
+        var secondCell = t.contents().eq(2);
+        var href= secondCell.contents('a').attr('href');
+        console.log(href);
+        if (href.charAt(href.length-1) != '/') {
+            $(this).attr('target', '_blank');
+        }});*/
+
+    //Convert the file info table into a dataTable 
+    $("table:first").dataTable({
+          "sDom": '<"top"pi>rt<"bottom"fl<"clear">',
+          "sPaginationType": "full_numbers",
+          "bStateSave": true,
+        });
+
+    //Prepare the popup for template description
+    $(".moaTemplateDescription").jqm({
+        overlay: 0, trigger: false,})
+    .jqmAddTrigger($(".moaTemplate"))
+    .jqmAddClose(".moaTemplateDescription");
+
+    //add a few triggers to the message div
+    $(".moaMessage")
+        .mouseenter(
+            function() {$(".moaMessage").hide();});
+                      
+//Init the menu
+    $('#jsddm > li').bind('mouseover', jsddm_open);
+	$('#jsddm > li').bind('mouseout',  jsddm_timer);
+
+    document.onclick = jsddm_close;
+    
+});
