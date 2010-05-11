c807a4c0396a9821092572f954b503e9b705515b
Mark Fiers <mark.fiers@plantandfood.co.nz>
2010-05-11 14:40:25 +1200

    shortcut for adhoc

---
+++ doc/markdown/commands/a.md
@@ -0,0 +1,5 @@
+`moa a` is a shortcut for (quickly) creating adhoc jobs. It's base usage is:
+
+
+    moa a 'cp ../testfile .'
+
---
+++ lib/python/moa/plugin/adhoc.py
@@ -0,0 +1,120 @@
+# 
+# Copyright 2009 Mark Fiers, Plant & Food Research
+# 
+# This file is part of Moa - http://github.com/mfiers/Moa
+# 
+# Moa is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+# 
+# Moa is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+# License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with Moa.  If not, see <http://www.gnu.org/licenses/>.
+# 
+
+"""
+Adhoc - some utilities to quickly create adhoc jobs
+"""
+
+import os
+import sys
+import optparse
+
+import moa.job
+import moa.logger
+l = moa.logger.l
+
+def defineCommands(commands):
+    commands['a'] = { 
+        'desc' : 'Quickly create an adhoc analysis',
+        'call' : createAdhoc
+        }
+
+def defineOptions(parser):
+    parserN = optparse.OptionGroup(parser, "Moa adhoc (a)")
+    parser.set_defaults(mode="seq", directory=".")
+    try:
+        parserN.add_option("-t", "--title", dest="title", help="Job title")
+        parserN.add_option("-d", "--directory", 
+                           dest="directory",
+                           help="Directory to create the new template in (default: .)")
+    except optparse.OptionConflictError:
+        pass # probably already defined in the newjob plugin
+    parserN.add_option("-m", "--mode",
+                       dest="mode",
+                       help="Directory to create the new template in (default: .)")
+    parserN.add_option("-i", "--input",
+                       dest="input",
+                       help="Input files for this adhoc job")
+    parser.add_option_group(parserN)
+
+
+def createAdhoc(wd, options, args):
+    """
+    Create an adhoc job
+    """
+
+    command = ""
+    params = []
+
+    if len(args) > 0: command = args[0]
+    if len(args) > 1: params = args[1:]
+    
+    mode = None
+    if options.mode:        
+        if not options.mode in ['seq', 'par', 'all', 'simple']:
+            l.critical("Unknown adhoc mode: %s" % options.mode)
+            sys.exit(-1)        
+        mode = options.mode
+    elif '$<' in command:
+        mode = 'seq'
+        l.info("Setting adhoc mode to 'seq', change to 'par' if you are confident ")
+        l.info("that parallel operation is possible")
+    elif ('$^' in command) or ('$?' in command):
+        mode = 'all'
+        l.info("Setting mode to 'all' - processing all files in one go")
+    else:
+        mode = 'simple'
+        l.info('did not see $? or $^ in the command line - assuming no input files')
+
+    params += ['adhoc_mode=%s' % options.mode]
+
+    if options.input:
+        l.debug("interpreting adhoc input: %s" % options.input) 
+        i = options.input
+        if i[-1] == '/':
+            p = 'adhoc_input_dir=%s' % i
+            params.append(p)
+            l.info('setting %s' % p)
+        else:
+            path, rest = i.rsplit('/', 1)
+            p = 'adhoc_input_dir=%s' % path
+            l.debug('setting %s' % p)
+            params.append(p)
+            l.info('setting %s' % p)
+            if '.' in rest:
+                glob, ext = rest.rsplit('.', 1)
+                if glob != '*':
+                    l.debug('setting glob to %s' % glob)
+                    params.append('adhoc_input_glob=%s' % glob)
+                l.debug('setting extension to %s' % ext)
+                params.append('adhoc_input_extension=%s' % ext)
+          
+    if command: 
+        params.append('adhoc_process=%s' % command)
+
+    l.debug('setting parameters %s' % params)
+    moa.job.newJob(template='adhoc',
+                   wd = wd,
+                   title = options.title,
+                   force = options.force,
+                   parameters=params)
+
+
+        
+
