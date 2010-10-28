87f3380689169536041dc332f902e94c83c4495b
Mark Fiers <mark.fiers.42@gmail.com>
2010-10-28 15:21:29 +1300

    Dummy backend for dirs without a job configured

---
+++ lib/python/moa/backend/nojob.py
@@ -0,0 +1,21 @@
+
+import moa.logger as l
+import moa.backend
+
+class NojobBackend(moa.backend.BaseBackend):
+    """
+    Nojob backend - placeholde for directories without a job
+    """    
+    def __init__(self, job):
+        """
+        Initialize this backend 
+        """
+        self.job = job
+        
+        
+    def isMoa(self):
+        """
+        Check if the current job is valid according to the 
+        backend type
+        """
+        return False
