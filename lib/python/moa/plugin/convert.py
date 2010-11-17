b639a68ac7c9d7ad9c62485e3763d100d2cbdd58
Mark Fiers <mark.fiers.42@gmail.com>
2010-11-18 03:07:57 +1300

    added support for filesets

--- lib/python/moa/plugin/convert.py
+++ lib/python/moa/plugin/convert.py
@@ -40,6 +40,10 @@ MAKEFILE_CONVERT_LINES = [
      'include $(MOABASE)/lib/gnumake/prepare.mk'),
     ('include $(MOABASE)/template/moa/core.mk',
      'include $(MOABASE)/lib/gnumake/core.mk'),
+    ('include $(shell echo $$MOABASE)/template/moa/prepare.mk',
+     'include $(MOABASE)/lib/gnumake/prepare.mk'),
+    ('include $(shell echo $$MOABASE)/template/moa/core.mk',
+     'include $(MOABASE)/lib/gnumake/core.mk'),
     ]
 
 MOABASE = moa.utils.getMoaBase()
@@ -55,8 +59,7 @@ def templateConvert(data):
     if len(args) < 1:
         l.critical("need to specify a target directory")
     newTemplateDir = args[0]
-    if len(args) > 1:
-        convert_only = args[1:]
+    convert_only = args[1:]
         
     if not os.path.exists(newTemplateDir):
         l.warning("creating directory %s" % newTemplateDir)
@@ -106,18 +109,12 @@ def templateConvert(data):
             newParName = p.replace('%s_' % moaId, '')
             if par.has_key('value'): del par['value']
             if par.has_key('cardinality'): del par['cardinality']
-            print p, newParName
-            for k in par:
-                print ' -', k, par[k]
+            #print p, newParName
+            #for k in par:
+            #    print ' -', k, par[k]
             inf['parameters'][newParName]= par
 
             
-        for k in  inf.keys():
-            print '.%s.' % k, "\t", str(inf[k])[:60]
-
-        with open(newTemplateConf, 'w') as F:
-            yaml.dump(inf, F)
-
         #convert gnumake file
         oldTemplateFile = os.path.join(MOABASE, 'template', '%s.mk' % job)
         with open(oldTemplateFile) as F:
@@ -131,17 +128,72 @@ def templateConvert(data):
                     break
 
         text = "".join(lines)
+
         #print text
+        #see if there are 'input filesets'
+        #$(call moa_fileset_define_opt,$(moa_id)_input,,Input files for $(moa_id))
+        rere = re.compile(r'\$\(call moa_fileset_define(.*),(.+),(.*),(.*)\)')
+        for x in rere.finditer(text):
+            fsid = x.groups()[1]
+            fsid = fsid.replace('$(moa_id)', moaId).replace(moaId + '_','')
+            fsdesc = x.groups()[3].replace('$(moa_id)', moaId)
+            l.info("found fileset for %s" % fsid)
+            if x.groups()[0] == '_opt':
+                fsopt = True
+            else:
+                fsopt = False
+            fs = { 'type' : 'input',
+                   'description' : fsdesc,
+                   'optional' : fsopt
+                  }
+            if not inf.has_key('filesets'):
+                inf['filesets'] = {}
+            inf['filesets'][fsid] = fs
+
+        text = rere.sub('', text)
+
+        # see if there are 'remap' filesets
+        rere = re.compile(r'\$\(call moa_fileset_remap(.*),(.+),(.*),(.*)\)')
+        for x in rere.finditer(text):
+            fsid = x.groups()[2].replace('$(moa_id)', moaId).replace(moaId + '_','')
+            fssrc = x.groups()[1].replace('$(moa_id)', moaId).replace(moaId + '_','')
+            fs_target_extension = x.groups()[3]
+            l.info("found a remap fileset: %s to %s" % (fssrc, fsid))
+            if x.groups()[0] == '_nodir':
+                fs_target_dir = '.'
+            else:
+                fs_target_dir = os.path.join('.', fs_target_extension)
+            fs = {'type' : 'map',
+                  'source' : fssrc,
+                  'dir' : fs_target_dir,
+                  'extension' : fs_target_extension
+                  }
+            if not inf.has_key('filesets'):
+                inf['filesets'] = {}
+            inf['filesets'][fsid] = fs
+
+        text = rere.sub('', text)
+        #rere = re.compile(r'(\$\(call.*)')
+        #for x in rere.finditer(text):
+        #    l.warning("found $call")
+        #    l.warning(x.groups()[0])
+
+        
         for suffix in ['title', 'help', 'type', 'define', 'description',
                        'prerequisites', 'default', 'allowed', 'set', 'simple']:
             rere = re.compile(r'\S+'+suffix+r' ?\+?=.*?(?<!\\)\n', re.S)
             text = rere.sub('', text)
-            #for x in rere.finditer(text):
-            #    print x.group(0)
-            #    print
 
         rere = re.compile(r'([\t ]*\n){2,}', re.S)
         text = rere.sub("\n\n", text)
+
+        #save & store the new files
+        thisTargetDir = os.path.dirname(newTemplateConf)
+        if not os.path.exists(thisTargetDir):
+            os.makedirs(thisTargetDir)
+
+        with open(newTemplateConf, 'w') as F:
+            yaml.dump(inf, F)
 
         with open(newTemplateFile, 'w') as G:
             G.write(text)
