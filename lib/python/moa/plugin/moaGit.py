78a88657217c428e9148318a8a4d32324c86fe8f
Mark Fiers <mark.fiers.42@gmail.com>
2011-04-14 08:53:22 +1200

    git is working again - currently a lightweight implementation

--- lib/python/moa/plugin/moaGit.py
+++ lib/python/moa/plugin/moaGit.py
@@ -16,11 +16,9 @@ import git
 import time
 import optparse
 
+from moa.sysConf import sysConf
 import moa.logger as l
 import moa.plugin.newjob
-
-GITROOT = None
-GITREPO = None
 
 def defineCommands(data):
     data['commands']['history'] = {
@@ -31,114 +29,77 @@ def defineCommands(data):
         'desc' : 'Tag the current version',
         'call': tag
         }
-
     
 def defineOptions(data):
     parserG = optparse.OptionGroup(
         data['parser'], 'Version control (Git)')
-    
-    parserG.add_option('--git-force-init', action='store_true',
-                       dest='gitForceInit',
-                      help = 'Force initialization of a new git repository, this ' +
-                      'deletes the old repository')
     parserG.add_option('--m', action='store',
                        dest='gitMessage', 
                       help = 'Commit message for git')
     
-    data['parser'].add_option_group(parserG)
+    data.parser.add_option_group(parserG)
 
-def tag(data):
-    global GITREPO
-    if not GITREPO:
-        l.info("no repository is initialized")
+def _getRepo(job):
+    """
+    Return the git repository object
+    """
+    wd = job.wd
+    try:
+        return git.Repo(wd)
+    except git.InvalidGitRepositoryError:
+        return None
+
+def _commit(job, message):
+    repo = _getRepo(job)
+    if not repo:
+        return
+        
+    l.debug("Found git repo: Commiting")
+    repo.index.add(job.getFiles())
+    repo.index.commit(message)
+
+    
+def tag(job):
+    repo = _getRepo(job)
+    if not repo:
+        l.debug("no repository is initialized")
         return
 
-    tagname = data['args'][1]
-    message = data['options'].gitMessage
+    tagname = sysConf.args[1]
+    message = sysConf.options.gitMessage
     l.info('tagging with "%s"' % tagname)
-    GITREPO.create_tag(tagname, message=message)
-
-def prepare(data):
-    #check if we're inside a git repository
-    if not data.has_key('projectRoot'):
-        return
-
-    projectRoot = data['projectRoot']
-    l.debug("seeing if %s is a git repos" % projectRoot)
-    repo = git.Repo(projectRoot)
-    
-    global GITROOT
-    global GITREPO
-    GITROOT = projectRoot
-    GITREPO = repo
-    
-    l.debug("found a git root at %s " % GITROOT)
+    repo.create_tag(tagname, message=message)
 
 def postSet(data):
     """
     Execute just after setting a parameter
     """
-    global GITROOT
-    global GITREPO
-    #try to commit this project
-    if not GITROOT:
-        #no git repository - not registering any files
-        return
-
-    l.debug("committing postset")
-    index = GITREPO.index
-    index.commit('Set %s ' % " ".join(data['newargs']))
-    
+    job = data.job
+    _commit(job, 'moa set %s in %s' % (
+        " ".join(data['newargs']), job.wd))
     
 def postNew(data):
     """
     To be executed just after the 'moa new' command
     """    
-    wd = data['cwd']
-    global GITROOT
-    global GITREPO
+    job = data.job
+    _commit(job, "created job %s in %s" % (job.template.name, job.wd))
 
-    l.debug("running git post new hook")
-    l.debug('GITROOT %s' % GITROOT)
-
-    template = 'traverse'
-    for a in data['newargs']:
-        if '=' in a: continue
-        template = a
-        
-    if template == 'project':
-        # creating new project - see if we need to create a
-        # new repository
-        return _gitInit(data)
-
-    #try to commit this project
-    if not GITROOT:
-        #no git repository - not registering any files
-        return
-    
-    info = moa.info.info(wd)
-    index = GITREPO.index
-    index.add(info['moa_files'].split())
-    index.commit('Inital commit of job "%s"' % info['title'])
-
-def gitlog(data):
+def gitlog(job):
     """
     Print a log to screen
     """
-    global GITREPO
-    global GITROOT
-
-    if not GITREPO:
-        l.info("noting to report - no repo")
+    repo = _getRepo(job)
+    if not repo:
+        l.info("noting to report - no git repository found")
         return
 
     tags = {}
     
-    for t in GITREPO.tags:
-        print t.commit
+    for t in repo.tags:
         tags[t.commit] = t
 
-    for c in GITREPO.iter_commits():
+    for c in repo.iter_commits():
         #if str(c) in tags.keys()
         t = time.strftime("%d %b %Y %H:%M", time.localtime(c.authored_date))
 
@@ -146,34 +107,3 @@ def gitlog(data):
             print " tag| %s" % tags[c]
         
         print "%3s | %s | %s" % (c.count(), t, c.message)
-    
-def _gitInit(data):
-    """
-    Initialize a git repository
-    """    
-    global GITREPO
-    global GITROOT
-    if GITROOT:
-        l.critical("trying to initialize a git repository wihtin a git repository")
-        sys.exit(-1)
-
-    wd = data['cwd']
-    
-    GITROOT = wd
-    GITREPO =  git.Repo.init(GITROOT)
-    l.info("created a git repository at %s" % GITROOT)
-    
-    info = moa.info.info(wd)
-    index = GITREPO.index
-    index.add(info['moa_files'].split())
-    index.commit('Settin up project "%s"' % info['title'])
-
-    #write some data to .gitignore
-    with open(os.path.join(wd, '.gitignore'), 'w') as F:
-        F.write(".*\n")
-        F.write("moa.success\n")
-        F.write("moa.out\n")
-        F.write("moa.err\n")
-        F.write("moa.failed\n")
-        F.write("moa.runlock\n")
-        
