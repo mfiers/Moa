40a351dc8aed27c9e1f47f2ee7e03e95b2c82cd2
Mark Fiers <mark.fiers@plantandfood.co.nz>
2011-08-03 10:25:29 +1200

    lots of cleaning - sge actor now holds run & finish jobs until prepare & run is done

--- lib/python/moa/plugin/sgeActor/__init__.py
+++ lib/python/moa/plugin/sgeActor/__init__.py
@@ -21,21 +21,11 @@ from moa.sysConf import sysConf
 import moa.logger as l
 import moa.plugin.newjob
 
-## def hook_defineCommands():
- #    sysConf['commands']['history'] = {
- #        'desc' : 'display a version control log',
- #        'call': gitlog
- #        }
- #    sysConf['commands']['tag'] = {
- #        'desc' : 'Tag the current version',
- #        'call': tag
- #        }
-
 def hook_defineOptions():
     sysConf.parser.add_option( '--sge', action='store_const', const='sge',
                                dest='actorId', help='Use SGE as actor' )
 
-def sgeRunner(wd, cl, conf={}):
+def sgeRunner(wd, cl, conf={}, **kwargs):
     """
     Run the job using SGE
 
@@ -45,7 +35,12 @@ def sgeRunner(wd, cl, conf={}):
     - store stdout & stderr in log files
     - return the rc
     """
-    
+
+    #see if we can get a command
+    command = kwargs.get('command', 'unknown')
+    if command == 'unknown':
+        l.error("runner should be called with a command")
+
     outDir = os.path.join(wd, '.moa', 'log.latest')
     if not os.path.exists(outDir):
         try:
@@ -65,9 +60,30 @@ def sgeRunner(wd, cl, conf={}):
     qcl.append(errfile)
     qcl.append('-o')
     qcl.append(outfile)
+
+    if command == 'run':
+        prep_jids = sysConf.job.data.sge.jids.get('prepare', [])
+        #hold until the 'prepare' jobs are done
+        if prep_jids: 
+            qcl.append('-hold_jid')
+            qcl.append(','.join(map(str, prep_jids)))
+    elif command == 'finish':
+        run_jids = sysConf.job.data.sge.jids.get('run', [])
+        #hold until the 'prepare' jobs are done
+        if run_jids: 
+            qcl.append('-hold_jid')
+            qcl.append(','.join(map(str, run_jids)))
+
+    #give it a reasonable name
+    if conf.get('runid', None):
+        qcl.append('-N')
+        qcl.append(str(conf['runid']))
+
+    # make sure the environment is copied
     qcl.append('-V')
     qcl.extend(cl)
 
+    print " ".join(qcl)qsta
     #dump the configuration in the environment
     for k in conf:
         # to prevent collusion, prepend all env variables
@@ -85,17 +101,21 @@ def sgeRunner(wd, cl, conf={}):
     moa.ui.message("executing %s" % " ".join(qcl))
     p = subprocess.Popen(qcl, cwd = wd, stdout=subprocess.PIPE)
     o, e = p.communicate()
-    jid = o.strip().split()[2]
+    jid = int(o.strip().split()[2])
     sysConf.actor.sge.jids.append(jid)
     moa.ui.message("submitted job with sge job id %s " % jid)
 
+    #store the job id submitted
+    if not sysConf.job.data.sge.jids.has_key(command):
+            sysConf.job.data.sge.jids[command] = []
+    sysConf.job.data.sge.jids[command].append(jid)
     return p.returncode
 
 def hook_postRun():
     """
     Need to exit here, and reconvene once all jobs have executed
     """
-    print sysConf.actor.sge.jids
+    #print sysConf.actor.sge.jids
 
 sysConf.actor.actors['sge'] = sgeRunner
 sysConf.actor.sge.jids = []
