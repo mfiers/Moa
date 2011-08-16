fd66d120a2cfaf796f1a622ed148d6e802d2e271
Mark Fiers <mark.fiers.42@gmail.com>
2011-08-16 13:08:38 +1200

    added lockfile as a prerequisite

--- setup.py
+++ setup.py
@@ -85,7 +85,8 @@ setup(name='moa',
           'Yaco>=0.1.7',
           'fist>=0.1.2',
           'unittest2>=0.5',
-          'lockfile>=0.9'
+          'lockfile>=0.9',
+          'mdGraph>=0.1'
           ],
       classifiers = [
           'Development Status :: 4 - Beta',
