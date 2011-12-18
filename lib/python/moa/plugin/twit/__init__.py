aed07a428aea0e23786f7efbfbe135498c0ca896
Mark Fiers <mark.fiers@plantandfood.co.nz>
2011-12-19 11:06:23 +1300

    Do not crash if twitter module is not installed

--- lib/python/moa/plugin/twit/__init__.py
+++ lib/python/moa/plugin/twit/__init__.py
@@ -14,23 +14,29 @@ Use twitter to send a message upon job completion
 
 import os
 import time
-import twitter
+
+try:
+    import twitter
+except ImportError:
+    twitter = False
+
 from moa.sysConf import sysConf
 
 def postRun(job):
     """
     Send a tweet out upon completing the default run
     """
-    #this is a pain :(
-    api = twitter.Api(
-        consumer_key =  sysConf.plugins.twit.consumer_key,
-        consumer_secret =  sysConf.plugins.twit.consumer_secret,
-        access_token_key = sysConf.plugins.twit.access_token_key,
-        access_token_secret = sysConf.plugins.twit.access_token_secret,
-        base_url='http://twitter.com')
+    if twitter != None:
+        #this is a pain :(
+        api = twitter.Api(
+            consumer_key =  sysConf.plugins.twit.consumer_key,
+            consumer_secret =  sysConf.plugins.twit.consumer_secret,
+            access_token_key = sysConf.plugins.twit.access_token_key,
+            access_token_secret = sysConf.plugins.twit.access_token_secret,
+            base_url='http://twitter.com')
 
-    api.PostUpdate('Finished %s in %s on %s' % (
-            (" ".join(sysConf.args),
-             os.path.basename(sysConf.job.wd),
-             time.ctime())))
+        api.PostUpdate('Finished %s in %s on %s' % (
+                (" ".join(sysConf.args),
+                 os.path.basename(sysConf.job.wd),
+                 time.ctime())))
 
