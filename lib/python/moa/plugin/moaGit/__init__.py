b07a80b49d9aa8ad3713b4b613124ae31e57aa53
Mark Fiers <mark.fiers@plantandfood.co.nz>
2012-01-03 15:50:36 +1300

    git support seems to work

--- lib/python/moa/plugin/moaGit/__init__.py
+++ lib/python/moa/plugin/moaGit/__init__.py
@@ -22,36 +22,119 @@ import moa.logger as l
 import moa.plugin.newjob
 
 def hook_defineCommands():
-    sysConf['commands']['history'] = {
-        'desc' : 'display a version control log',
+    sysConf['commands']['gitlog'] = {
+        'desc' : 'display a nicely formatted git log',
         'call': gitlog
         }
-    sysConf['commands']['tag'] = {
+    sysConf['commands']['gittag'] = {
         'desc' : 'Tag the current version',
         'call': tag
         }
-    
+    sysConf['commands']['gitadd'] = {
+        'desc' : 'Add the current job to the git repository',
+        'call': gitadd
+        }
+
+
 def _getRepo(job):
     """
     Return the git repository object
     """
     wd = job.wd
     try:
-        return git.Repo(wd)
+        repo = git.Repo(wd)
+        return repo
     except git.InvalidGitRepositoryError:
         return None
 
-def _commit(job, message):
-    repo = _getRepo(job)
-    if not repo:
-        if sysConf.plugin_settings.moaGit.warnings:
-            moa.ui.message("git plugin is active, but no repository found")
-        return
-        
-    l.debug("Found git repo: Commiting")
-    repo.index.add(job.getFiles())
-    repo.index.commit(message)
+
+def _realCommit(repo, files, wd, message):
+    """
+    Do the actual commit
+    """
+    message += "\n\n"
+    message += "Committed by the moa/ moaGit plugin\n"
+    message += "work directory: %s\n" % wd
+    message += "command line:\n"
+    message += "   %s\n" % " ".join(sys.argv)
+
+    repo.index.add(map(os.path.abspath, files))
+
+    if (len(repo.heads) == 0) or (repo.is_dirty()):
+        moa.ui.message("Committing changes to git")
+        repo.index.commit(message)
+    else:
+        l.debug("Skipping git add - repository is not dirty")
+
+def _checkGitIgnore(gitignoreFile):
+    """
+    See if there is moa dir specific git ignore file - if not create one
+    """
+    if not os.path.exists(gitignoreFile):
+        with open(gitignoreFile, 'w') as F:
+            F.write("log\n")
+            F.write("log.d/\n")
+            F.write("log.latest\n")
+            F.write("last_run_id\n")
+            F.write("status\n")
+            F.write("*.fof\n")
+            F.write("*~\n")
+            
+def _commitDir(wd, message):
+    """
+    Simpler utility to add files to a repository without
+    initiating a moa job object
+    """
+    repo = sysConf.git.repo
+    if not repo: return
+
     
+    moadir = os.path.join(wd, '.moa')
+    if not os.path.exists(moadir):
+        return
+    
+    gitignoreFile = os.path.join(wd, '.moa', '.gitignore')
+    _checkGitIgnore(gitignoreFile)
+
+    files = set([gitignoreFile])
+    for gl in ['.moa/template',
+               '.moa/template.d/*',
+               '.moa/config',
+               'moa.*',
+               'Readme', 'README', 'Readme.*',
+               'Changelog', 'CHANGELOG', 'Changelog.*' ]:
+        
+        for f in glob.glob(os.path.join(wd, gl)):
+            files.update(set(glob.glob(os.path.join(wd, gl))))
+            
+        remove = [x for x in files if x[-1] == '~']
+        files.difference_update(remove)
+
+    _realCommit(repo, list(files), wd, message)
+    #repo.git.commit(os.path.join(wd, '.moa', 'template.d'))
+    
+def _commit(job, message):
+    """
+    Commit the current job to the repository
+    """
+    repo = sysConf.git.repo
+    if not repo: return
+    
+    gitignoreFile = os.path.join(job.wd, '.moa', '.gitignore')
+    _checkGitIgnore(gitignoreFile)
+
+    files = [gitignoreFile]
+    files.extend(job.getFiles())
+    _realCommit(repo, files, job.wd, message)
+    #repo.git.commit(os.path.join(job.wd,  '.moa', 'template.d'))
+                    
+def gitadd(job):
+    """
+    Add a job to the git repository
+    """
+    l.debug("adding to git %s" % job.wd)
+    _commit(job, "add/refresh of %s" % job.wd)
+
 def tag(job):
     repo = _getRepo(job)
     if not repo:
@@ -63,47 +146,25 @@ def tag(job):
     l.info('tagging with "%s"' % tagname)
     repo.create_tag(tagname, message=message)
 
-def hook_postSet():
+def hook_prepare_3():
     """
-    Execute just after setting a parameter
+    Register a function for submitting a job
     """
-    job = sysConf.job
-    _commit(job, 'moa set %s in %s' % (
-        " ".join(sysConf['newargs']), job.wd))
-    
-def hook_postNew():
-    """
-    To be executed just after the 'moa new' command
-    """    
-    job = sysConf.job
-    _commit(job, "Created job %s in %s" % (job.template.name, job.wd))
+    sysConf.git.commit = _commit
+    sysConf.git.active = True
+    sysConf.git.commitJob = _commit
+    sysConf.git.commitDir = _commitDir
+    sysConf.git.getRepo = _getRepo
+    sysConf.git.repo = _getRepo(sysConf.job)
+    if not sysConf.git.repo and sysConf.plugin_settings.moaGit.warn:
+            moa.ui.warn("Cannot find a git repository!")
 
-def _registerFiles(job):
+def hook_finish():
     """
-    Register the files in `job` for version control!    
+    Handle all git changes post execution - actually defer to plugin specific calls
     """
-    repo = _getRepo(job)
-    index = repo.index
-    for pattern in job.data.moaFiles:
-        for ff in  glob.glob(os.path.join(job.wd, pattern)):
-            print ff
-            index.add('./' + ff)
-
-def hook_postCp():
-    """
-    after 'moa cp'
-    """
-    #need to restore this -others might want it!
-    rememberCurrentJob = sysConf.job
-    
-    for wd in sysConf.moautil.jobs_created:
-        job = moa.job.Job(wd)
-        _registerFiles(job)
-    
-    
-    
-    #_commit(job, "Copied job from %s in %s" % (job.template.name, job.wd))
-    
+    sysConf.pluginHandler.run('git_finish_%s' % sysConf.originalCommand)
+        
 def gitlog(job):
     """
     Print a log to screen
@@ -124,4 +185,4 @@ def gitlog(job):
         if c in tags.keys():
             print " tag| %s" % tags[c]
         
-        print "%3s | %s | %s" % (c.count(), t, c.message)
+        print "%3s | %s | %s" % (c.count(), t, c.message.split("\n")[0])
