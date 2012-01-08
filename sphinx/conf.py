56a485bb0968e12955f5198f64b88299ddb6ffcc
Mark Fiers <mark.fiers@plantandfood.co.nz>
2012-01-09 10:48:11 +1300

    minor changes

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -115,7 +115,7 @@ html_theme_path = ["."]
 #html_title = None
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-html_short_title = 'Moa'
+#html_short_title = 'Moa'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
