5d98fddeece4781aeeb8c02779f177339dcc5474
Mark Fiers <mark.fiers.42@gmail.com>
2012-06-15 13:00:14 +1200

    uses TTYtter as a backend now

--- lib/python/moa/plugin/system/twit.py
+++ lib/python/moa/plugin/system/twit.py
@@ -10,33 +10,72 @@
 ------------------------
 
 Use twitter to send a message upon job completion
+
+Depends on an configured TTYter installation in the path.
+see: http://www.floodgap.com/software/ttytter/
+
 """
 
 import os
 import time
-
-try:
-    import twitter
-except ImportError:
-    twitter = False
+import socket
+from datetime import datetime, timedelta
+import subprocess as sp
 
 from moa.sysConf import sysConf
 
-def postRun(job):
+def niceRunTime(d):
+    """
+    Nice representation of the run time
+    d is time duration string    
+    """
+    d = str(d)
+    if ',' in d:
+        days, time = d.split(',')
+    else:
+        days = 0
+        time = d
+
+    hours, minutes, seconds = time.split(':')
+    hours, minutes = int(hours), int(minutes)
+    seconds, miliseconds = seconds.split('.')
+    seconds = int(seconds)
+    miliseconds = int(miliseconds)
+    
+    if days > 0:
+        if days == 1:            
+            return "1 day, %d hrs" % hours
+        else:
+            return "%d days, %d hrs" % (days, hours)
+        
+    if hours == 0 and minutes == 0 and seconds == 0:
+        return "<1 sec"
+    if hours > 0:
+        return "%d:%02d hrs" % (hours, minutes)
+    elif minutes > 0:
+        return "%d:%02d min" % (minutes, seconds)
+    else:
+        return "%d sec" % seconds
+
+def hook_postRun():
     """
     Send a tweet out upon completing the default run
     """
-    if twitter != None:
-        #this is a pain :(
-        api = twitter.Api(
-            consumer_key =  sysConf.plugins.twit.consumer_key,
-            consumer_secret =  sysConf.plugins.twit.consumer_secret,
-            access_token_key = sysConf.plugins.twit.access_token_key,
-            access_token_secret = sysConf.plugins.twit.access_token_secret,
-            base_url='http://twitter.com')
+    runtime =  datetime.today() - sysConf.logger.start_time    
+    #print runtime
+    #print timedelta(seconds=1)
+    if timedelta(seconds=sysConf.plugins.system.twit.mintime) > runtime:
+        # do not return anythin unless it ran for at least a certain
+        # amount of time
+        return 
 
-        api.PostUpdate('Finished %s in %s on %s' % (
-                (" ".join(sysConf.args),
-                 os.path.basename(sysConf.job.wd),
-                 time.ctime())))
+    message = '\dm %s moa job in %s:%s finished in %s' % (
+        sysConf.plugins.system.twit.username,
+        socket.gethostname().split('.')[0],
+        sysConf.job.wd,
+        niceRunTime(runtime))
+    
+    #start TTYter process
+    P = sp.Popen(['ttytter', '-script'], stdin=sp.PIPE)
+    P.communicate(message)
 
