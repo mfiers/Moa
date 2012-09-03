11f397bc57be55b1d390c5fff20fcc4940ca9af3
Mark Fiers <mark.fiers@plantandfood.co.nz>
2012-09-03 15:22:33 +1200

    minor changes & optimization

--- lib/python/moa/logger.py
+++ lib/python/moa/logger.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 # Copyright 2009-2011 Mark Fiers
 # The New Zealand Institute for Plant & Food Research
-# 
+#
 # This file is part of Moa - http://github.com/mfiers/Moa
-# 
+#
 # Licensed under the GPL license (see 'COPYING')
-# 
+#
 import os
 import sys
 import time
@@ -13,38 +13,47 @@ import logging
 from logging import DEBUG, INFO, WARNING, CRITICAL
 
 import traceback
-LOGGER  = logging.getLogger('moa')
+LOGGER = logging.getLogger('moa')
 
 handler = logging.StreamHandler()
 
-logmark = chr(27) + '[0;45mMOA' + \
-          chr(27) + '[0m ' 
+logmark = chr(27) + '[0;45mMOA' + chr(27) + '[0m'
 
-logging.basicConfig(format=logmark + '%(levelname)s|%(asctime)s|%(name)s # %(message)s', datefmt='%y/%m/%d %H:%M:%S')
+logging.basicConfig(format=logmark +
+                    ' %(name)s - %(message)s',
+                    datefmt='%y/%m/%d %H:%M:%S')
+
 
 def exitError(message=""):
-    l.critical("Deprecated function call - do not use logger.exitError, but moa.ui.exitError")
+    l.critical("Deprecated function call - do not " +
+               "use logger.exitError, but moa.ui.exitError")
     if message:
         LOGGER.fatal(message)
     sys.exit(-1)
 
+
 def setLevel(level):
     if level == logging.DEBUG:
-        LOGGER.setLevel(logging.DEBUG)        
+        LOGGER.setLevel(logging.DEBUG)
     else:
         LOGGER.setLevel(level)
-    
+
+
 def setVerbose():
     LOGGER.setLevel(logging.DEBUG)
+
 
 def setSilent():
     LOGGER.setLevel(logging.CRITICAL)
 
+
 def setWarning():
     LOGGER.setLevel(logging.WARNING)
 
+
 def setInfo():
     LOGGER.setLevel(logging.INFO)
+
 
 def _callLogger(logFunc, args, kwargs):
     if len(args) == 1:
@@ -55,17 +64,22 @@ def _callLogger(logFunc, args, kwargs):
         for k in kwargs.keys():
             logFunc(" - %s : %s" % (k, kwargs[k]))
 
+
 def getLogger(name):
     return logging.getLogger(name)
-    
+
+
 def debug(*args, **kwargs):
     _callLogger(LOGGER.debug, args, kwargs)
+
 
 def info(*args, **kwargs):
     _callLogger(LOGGER.info, args, kwargs)
 
+
 def warning(*args, **kwargs):
     _callLogger(LOGGER.warning, args, kwargs)
+
 
 def error(*args, **kwargs):
     _callLogger(LOGGER.error, args, kwargs)
@@ -73,5 +87,3 @@ def error(*args, **kwargs):
 
 def critical(*args, **kwargs):
     _callLogger(LOGGER.critical, args, kwargs)
-
-    
