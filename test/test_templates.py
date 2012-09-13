656bca509efe2c71f016e9ab05c95f4ff2bf3f32
Mark Fiers <mark.fiers@plantandfood.co.nz>
2012-09-13 15:04:53 +1200

    unittest work again

--- test/test_templates.py
+++ test/test_templates.py
@@ -1,33 +1,35 @@
 import random
-import unittest2 as unittest
+import unittest
 import doctest
 
 import moa.job
 import moa.plugin
 import moa.template
 
+## Initialize the logger
+import moa.logger
+l = moa.logger.getLogger(__name__)
+
 import moa.backend.ruff.test
-#import moa.backend.gnumake.test
+
 
 from moa.sysConf import sysConf
-sysConf.initialize()
+sysConf.pluginHandler = moa.plugin.PluginHandler(sysConf.plugins.system)
+
 
 def load_tests(loader, tests, ignore):
     tests.addTests(templateTestSuite())
     return tests
 
+
 def templateTestSuite():
     suite = unittest.TestSuite()
-    for template in moa.template.templateList():
-        job = moa.job.newTestJob(template)
+    for provider, template in moa.template.templateList():
+        job = moa.job.newTestJob(template, provider=provider)
         if job.template.backend == 'ruff':
-            continue
-        
-            #tester = moa.backend.ruff.test.templateTest()
-            #tester.setJob(job)
-            #suite.addTest(tester)
-            
+            test = moa.backend.ruff.test.templateTest(job)
+            suite.addTest(test)
     return suite
-    
+
 if __name__ == '__main__':
     unittest.main()
