aeae2e6b12c0fa09a2b02f2ca25f811e41fc1353
Mark Fiers <mark.fiers@plantandfood.co.nz>
2012-09-13 14:29:51 +1200

    fixed few unittests

--- lib/python/moa/template/template.py
+++ lib/python/moa/template/template.py
@@ -1,10 +1,10 @@
 # Copyright 2009-2011 Mark Fiers
 # The New Zealand Institute for Plant & Food Research
-# 
+#
 # This file is part of Moa - http://github.com/mfiers/Moa
-# 
+#
 # Licensed under the GPL license (see 'COPYING')
-# 
+#
 """
 moa.template.template
 ---------------------
@@ -22,21 +22,21 @@ import Yaco
 import moa.logger as l
 import moa.template
 
+
 class Template(Yaco.Yaco):
     """
-    Template extends Yaco    
-    
+    Template extends Yaco
     """
-    
+
     def __init__(self, templateFile):
         """
         Initialze the template object, which means:
-        
+
         * Check if the template exists, if not raise an Exception
         * Load template info
 
         >>> import moa.job
-        >>> job = moa.job.newTestJob(template='adhoc')
+        >>> job = moa.job.newTestJob(template='simple')
         >>> tfile = os.path.join(job.confDir, 'template')
         >>> t = Template(tfile)
         >>> assert(isinstance(t, Yaco.Yaco))
@@ -51,21 +51,19 @@ class Template(Yaco.Yaco):
         #set a few defaults to be used by each template
         self.parameters = {}
         self.parameters.default_command = {
-            'default' : 'run',
-            'help' : 'command to run for this template',
-            'optional' : True,
-            'private' : True,
-            }
-        
+            'default': 'run',
+            'help': 'command to run for this template',
+            'optional': True,
+            'private': True}
+
         self.parameters.jobid = {
-            'help' : 'Identifier for this job - Should unique in the' +
-               'context of this workflow',
-            'optional' : True,
-            'recursive' : False,
-            'type' : 'string',
-            'default' : 'unset'
-            }
-        
+            'help': 'Identifier for this job - Should unique in the' +
+            'context of this workflow',
+            'optional': True,
+            'recursive': False,
+            'type': 'string',
+            'default': 'unset'}
+
         self.filesets = {}
 
         #try to load the template!!
@@ -77,16 +75,15 @@ class Template(Yaco.Yaco):
             noTemplate = True
         else:
             _tempTemplate = open(self.templateFile).read().strip()
-            if len(_tempTemplate) < 50 and \
-                   not "\n" in _tempTemplate:
+            if len(_tempTemplate) < 50 and not "\n" in _tempTemplate:
                 if os.access(self.templateFile, os.W_OK):
-                    #this must be an old style template name- try to load the template
+                    # this must be an old style template name-
+                    # try to load the template
                     moa.template.installTemplate(
                         os.path.dirname(os.path.dirname(templateFile)),
                         _tempTemplate)
                 else:
                     noTemplate = True
-
 
         if noTemplate:
             self.name = 'nojob'
@@ -101,7 +98,6 @@ class Template(Yaco.Yaco):
             original.load(self.templateFile)
             self.original = original
 
-
         l.debug("set template to %s, backend %s" % (self.name, self.backend))
         if not self.name == 'nojob' and not self.modification_date:
             self.modification_date = os.path.getmtime(self.templateFile)
@@ -111,9 +107,9 @@ class Template(Yaco.Yaco):
         Return a Yaco representation of the yaml-template, without any
         of this Template processing. This is really useful when
         processing a template that needs to be written back to disk
-        
+
         >>> import moa.job
-        >>> job = moa.job.newTestJob(template='adhoc')
+        >>> job = moa.job.newTestJob(template='simple')
         >>> raw = job.template.getRaw()
         >>> assert(isinstance(raw, Yaco.Yaco))
         >>> assert(raw.has_key('parameters'))
@@ -121,10 +117,9 @@ class Template(Yaco.Yaco):
         y = Yaco.Yaco()
         y.load(self.templateFile)
         return y
-    
+
     def saveRaw(self, raw):
         raw.save(self.templateFile)
-    
+
     def save(self):
         raise Exception("direct saving of template files is disabled")
-        
