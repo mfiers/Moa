46340eba177cb13bdd6bf448921269bc27f52e67
Mark Fiers <mark.fiers@plantandfood.co.nz>
2012-11-13 12:31:06 +1300

    define pud & ppud as system defined

--- moa/plugin/system/project.py
+++ moa/plugin/system/project.py
@@ -88,6 +88,7 @@ def hook_prepare_3():
                 'the context of this job',
         'recursive': False,
         'private': True,
+        'system': True,
         'type': 'string'
     }
 
@@ -96,6 +97,7 @@ def hook_prepare_3():
         'help': 'A unique project job identifier - only unique in ' +
                 'the context of this job',
         'recursive': False,
+        'system': True,
         'private': True,
         'type': 'string'
     }
